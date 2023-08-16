549f18b62b975977b9cb0f0fe095a91b417eddac
Yasuo Hayashibara <yasuo.hayashibara@it-chiba.ac.jp>
2023-08-16 09:20:20 +0900

    executable

--- rl_games/envs/humanoid/GankenKunEnv.py
+++ rl_games/envs/humanoid/GankenKunEnv.py
@@ -5,6 +5,8 @@ sys.path.append('./rl_games/envs/humanoid')
 import webots_client
 
 class GankenKunEnv(gym.Env):
+    DIRECTION = np.array([1,1,-1,-1,1,1,1,1,-1,1,1,-1,-1,1,1,1,1,-1,1])
+    OFFSET = np.array([0,0,-0.2,0.2,0,0,0,0,0,0,0,-0.2,0.2,0,0,0,0,0])
     def __init__(self, **kwargs):
         gym.Env.__init__(self)
         self.webots = webots_client.Webots()
@@ -15,19 +17,20 @@ class GankenKunEnv(gym.Env):
         self.action_space = gym.spaces.Box(low=-1, high=1, shape=(19, ), dtype=np.float32)
         self.observation_space = gym.spaces.Box(low=-6, high=6, shape=(32, ), dtype=np.float32)
     
-    def step(self, actions):
+    def step(self, action):
         self.walking_phase = (self.walking_phase+self.dt/self.walking_period)%1.0
-        self.walking_phase += actions[18]*self.dt/self.walking_period*0.5
+        self.walking_phase += action[18]*self.dt/self.walking_period*0.5
         #print("phase: "+str(self.walking_phase))
         right = 1-abs(2*((self.walking_phase*2)%1)*(self.walking_phase%1<0.5)-1)
         left = 1-abs(2*((self.walking_phase*2)%1)*(self.walking_phase%1>0.5)-1)
         #print("right: "+str(right)+", left: "+str(left))
-        actions[2] = -right
-        actions[3] = right
-        actions[11] = -left
-        actions[12] = left
-
-        self.webots.setAngle(actions)
+        action = self.DIRECTION * action
+        action[2] += right
+        action[3] -= right
+        action[11] += left
+        action[12] -= left
+        #print(action)
+        self.webots.setAngle(0.2 * action[0:18] + self.OFFSET)
         self.webots.measureSensors()
         state = []
         gravity_vec = [0, 0, -1]
@@ -43,12 +46,15 @@ class GankenKunEnv(gym.Env):
             pos = [blue1.position.X, blue1.position.Y, blue1.position.Z] 
             ball = self.webots.sensorMeasurements.object_positions[0]
             local_ball_pos = [ball.position.X - pos[0], ball.position.Y - pos[1], ball.position.Z - pos[2]]
+            print(ball.position)
 
         state += gravity_vec
-        state += actions
+        state += action.tolist()
         state += pos
         state += local_ball_pos
         state += projected_foward
+        state += [self.walking_phase]
+        state = np.array(state, dtype=np.float32)
 
         reward = 0
         done = False
